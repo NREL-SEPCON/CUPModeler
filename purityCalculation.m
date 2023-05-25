43b8955aba6741a5caf9b6733d4f95bac0f822e8
ManarAlherech <106359622+ManarAlherech@users.noreply.github.com>
2023-05-24 22:06:35 -0600

    Optimized purityCalculation function

--- purityCalculation.m
+++ purityCalculation.m
@@ -1,32 +1,28 @@
 function [Purity integralRanges] = purityCalculation(telute, Cout)
-
-    telute = telute';
     Cout = Cout';
-
     numCompounds = size(Cout, 2);
-    
-    integralRanges = repmat({{}}, numCompounds, 1);
-    Purity = zeros(1, numCompounds);
-
     Bot = inf;
-
+    
     for i = 1:numCompounds
         y = Cout(:, i);
-
+        
         if 0.005 * max(y) < Bot
             % Find the baseline (Bot) as a percentage of the peak height (1% = 0.01)
             Bot = 0.005 * max(y);
         end
     end
-
     
+    Purity = zeros(1, numCompounds);
+    telute = telute';
+    integralRanges = repmat({{0}}, numCompounds, 2);
+
     for i = 1:numCompounds
         totalImpurityArea = 0;
         y = Cout(:, i);
         ind = find(y > Bot);
-        
         yarea = trapz(telute(ind(1):ind(end)), y(ind(1):ind(end)));
-
+        integralRanges(i,1) = {telute(ind(1))};
+        integralRanges(i,2) = {telute(ind(end))};
         for j = 1:numCompounds
             if i ~= j
                 yImp = Cout(:, j);
@@ -35,7 +31,5 @@ function [Purity integralRanges] = purityCalculation(telute, Cout)
         end
         compoundPurity = (yarea / (yarea + totalImpurityArea)) * 100;
         Purity(i) = round(compoundPurity, 2);
-
     end
-
 end
